f4cca851ddf698882d750d9ccaefc01334f367b2
JoseMasiaTormo <josemasia2004@gmail.com>
2026-06-26 20:32:58 +0200

    first changes in router and app.vue

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,28 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import LoginView from "../views/LoginView.vue";
+import RegiterView from "../views/RegiterView.vue";
+import ArtistView from "../views/ArtistView.vue";
+import FavoritesView from "../views/FavoritesView.vue";
+import HistoryView from "../views/HistoryView.vue";
+
+const routes = [
+  { path: "/", component: HomeView, meta: { requiresAuth: true } },
+  { path: "/login", component: LoginView },
+  { path: "/register", component: RegiterView },
+  { path: "/artist/:id", component: ArtistView, meta: { requiresAuth: true } },
+  { path: "/favorites", component: FavoritesView, meta: { requiresAuth: true } },
+  { path: "/history", component: HistoryView, meta: { requiresAuth: true } },
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
-})
+  history: createWebHistory(),
+  routes,
+});
 
-export default router
+router.beforeEach((to) => {
+  const token = localStorage.getItem("token");
+  if (to.meta.requiresAuth && !token) return "/login";
+});
+
+export default router;
